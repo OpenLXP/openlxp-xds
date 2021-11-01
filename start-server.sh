537fa6313f26d02b67c9a6a6771be2147cb86db4
JDTobin <90791836+JDTobin@users.noreply.github.com>
2021-11-01 17:07:31 -0400

    Updates to use openlxp-authentication Adds support for using openlxp-authentication. Switches nginx/gunicorn proxy to use socket.

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd openlxp-xds; python manage.py createsuperuser --no-input)
 fi
-(cd openlxp-xds; gunicorn openlxp_xds_project.wsgi --reload --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd openlxp-xds; gunicorn openlxp_xds_project.wsgi --reload --user www-data --bind unix:/opt/xds.sock --workers 3) &
 nginx -g "daemon off;"
