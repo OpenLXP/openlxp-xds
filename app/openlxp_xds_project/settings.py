f6d16e2d3b58c67683a141bd9a54b3e5bf588114
KarenAJ <kjijo@deloitte.com>
2024-04-01 09:42:57 -0500

    Update settings.py

--- app/openlxp_xds_project/settings.py
+++ app/openlxp_xds_project/settings.py
@@ -186,8 +186,8 @@ LOGGING = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-# CSRF_COOKIE_DOMAIN = '.deloitteopenlxp.com'
-# CSRF_TRUSTED_ORIGINS = ['.deloitteopenlxp.com', ]
+CSRF_COOKIE_DOMAIN = '.deloitteopenlxp.com'
+CSRF_TRUSTED_ORIGINS = ['.deloitteopenlxp.com', ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
