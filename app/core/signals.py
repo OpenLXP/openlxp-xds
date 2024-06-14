4acb07e7fb8939146ffffc59188dc8d7f3eed7dd
kjijo <kjijo@deloitte.com>
2024-06-14 13:32:57 -0500

    added exception for email template

--- app/core/signals.py
+++ app/core/signals.py
@@ -1,3 +1,4 @@
+import logging
 from django.conf import settings
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
@@ -9,6 +10,8 @@ from openlxp_notifications.models import email
 
 from .models import InterestList
 
+logger = logging.getLogger('dict_config_logger')
+
 
 @receiver(m2m_changed, sender=InterestList.experiences.through)
 def interest_list_notify(sender, instance, action, reverse, pk_set, **kwargs):
@@ -17,19 +20,28 @@ def interest_list_notify(sender, instance, action, reverse, pk_set, **kwargs):
                     verb='experiences added', added=pk_set,
                     list_name=instance.name)
         #  setting variables for email request
+
         recipient_list = list(instance.subscribers.values_list(
             "email", "first_name", 'last_name'))
         owner = instance.owner
         list_name = instance.name
+
         if settings.LOGIN_REDIRECT_URL:
             list_url = (settings.LOGIN_REDIRECT_URL + "/"
                         + str(instance.id))
         else:
-            list_url = "ECC site under interest list " + instance.name
+            list_url = "ECC -> Subscribed Lists: " + instance.name
 
-        email_type = email.objects.get(
-                reference='Subscribed_list_update')
-        # trigger email notification
-        trigger_update(
-            email_type, recipient_list, owner,
-            list_name, list_url)
+        try:
+            email_type = email.objects.get(
+                    reference='Subscribed_list_update')
+
+            # trigger email notification
+            trigger_update(
+                email_type, recipient_list, owner,
+                list_name, list_url)
+        except email.DoesNotExist:
+            logger.error('Email configuration for subscribed list '
+                         'updates does not exist. Please add a '
+                         '"Subscribed_list_update" email template '
+                         'for the email alert. ')
