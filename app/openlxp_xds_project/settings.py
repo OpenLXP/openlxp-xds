f390fe9f3c93666a8874dc246a525dab4826accc
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-07-31 19:59:43 -0700

    upgraded django

--- app/openlxp_xds_project/settings.py
+++ app/openlxp_xds_project/settings.py
@@ -190,8 +190,8 @@ LOGGING = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_COOKIE_DOMAIN = '.deloitteopenlxp.com'
-CSRF_TRUSTED_ORIGINS = ['.deloitteopenlxp.com', ]
+CSRF_COOKIE_DOMAIN = os.environ.get('CSRF_COOKIE_DOMAIN')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
