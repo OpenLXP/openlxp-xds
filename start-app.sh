d9147db8ba46ba84044312186ee84c47437015b9
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-09-05 13:00:23 -0700

    adding csrf trusted origin/python collectstatic

--- start-app.sh
+++ start-app.sh
@@ -8,6 +8,7 @@ python manage.py loaddata admin_theme_data.json
 python manage.py loaddata openlxp_email_templates.json 
 python manage.py loaddata openlxp_email_subject.json 
 python manage.py loaddata openlxp_email.json 
+python manage.py collectstatic --no-input
 cd /opt/app/ 
 pwd 
 ./start-server.sh
