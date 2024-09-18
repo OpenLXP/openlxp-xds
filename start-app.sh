86c9071cdc5d332cd47392fb48fb78a32df034cb
kjijo <kjijo@deloitte.com>
2024-09-18 08:46:20 -0500

    Added Collectstatic

--- start-app.sh
+++ start-app.sh
@@ -4,6 +4,7 @@
 python manage.py waitdb 
 python manage.py migrate 
 python manage.py createcachetable 
+python manage.py collectstatic --no-input --clean
 python manage.py loaddata admin_theme_data.json 
 python manage.py loaddata openlxp_email_templates.json 
 python manage.py loaddata openlxp_email_subject.json 
