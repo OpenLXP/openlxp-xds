8e5fcdadb770475baaa79ae490c923c7b858209b
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-10-22 14:34:09 -0700

    Removing unused imports in urls.py

--- app/openlxp_xds_project/urls.py
+++ app/openlxp_xds_project/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, re_path, path
-from django.http import HttpResponse
+from django.urls import include, re_path
 
 
 # def health_check(request):
