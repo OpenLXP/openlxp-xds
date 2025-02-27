8371382415bc065f30391ea8a386bfacf74718c3
Milton Reder <milt@yetanalytics.com>
2025-02-27 11:43:35 -0500

    remove deprecated verbs as old clients don't know about the api

--- app/xds_api/xapi.py
+++ app/xds_api/xapi.py
@@ -3,13 +3,6 @@ import uuid
 
 
 VERB_WHITELIST = {
-    # DEPRECATED verbs we still let through
-    "https://w3id.org/xapi/tla/verbs/socialized",
-    "https://w3id.org/xapi/acrossx/verbs/explored",
-    "https://w3id.org/xapi/acrossx/verbs/prioritized",
-    "https://w3id.org/xapi/tla/verbs/registered",
-    "https://w3id.org/xapi/dod-isd/verbs/curated",
-    # Expected XDS-UI Instrumentation
     "https://w3id.org/xapi/tla/verbs/explored",
     "https://w3id.org/xapi/acrossx/verbs/searched",
     "http://activitystrea.ms/save",
